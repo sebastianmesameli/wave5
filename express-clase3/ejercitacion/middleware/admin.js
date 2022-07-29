60ee690d39ff7fbeba0f0f43e9692c9c588eab8e
sebastianMesa <sebastian.mesa@mercadolibre.com.co>
2022-07-28 20:17:31 -0500

    Express clase 3 completed

--- express-clase3/ejercitacion/middleware/admin.js
+++ express-clase3/ejercitacion/middleware/admin.js
@@ -7,19 +7,26 @@ las utilizamos en "/routes/products.js"
 que es res.locals? : https://www.geeksforgeeks.org/express-js-res-locals-property/
 */
 /**
- * Esta funcion deberia guardar en res.locals.user un objeto con atributos id,name,admin recibidos por body 
+ * Esta funcion deberia guardar en res.locals.user un objeto con atributos id,name,admin recibidos por body
  */
 
 function user(req, res, next) {
+  res.locals.user = {
+    id: 1,
+    name: "pepe",
+    admin: true,
+  };
+  next();
+}
 
-		next()
-	
-};
+function auth(req, res, next) {
+	const {name, admin} = res.locals.user;
+	if(admin){
+		res.send(`El usuario ${name}, es admin`);
+	} else {
+		res.send('Error Auth');
+	}
+  next();
+}
 
-
- function auth(req, res,next) {
-	
-	next()
-};
-
-module.exports = {user,auth}
+module.exports = { user, auth };
--- express-clase3/ejercitacion/routes/products.js
+++ express-clase3/ejercitacion/routes/products.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const router = express.Router();
 let brandsDB = require("../utils/products");
-const {user,auth} = require("../middleware/admin");
+const { user, auth } = require("../middleware/admin");
 
 /************************************************************* 
 	GET
 http://localhost:3000/api/product/Logitech/1
  ************************************************************/
 
-
-
 router.get("/product", (req, res) => {
-
+  res.json(brandsDB);
 });
-
 
 /**
 middleware a nivel de rutas: 
@@ -23,24 +20,34 @@ en este caso utilizamos las dos funciones que definimos en el archivo
 
  */
 // esto no tiene test
-router.get("/user",user,auth,(req,res)=>{
-
-})
-
-
-
-router.get("/product", (req, res) => {
-	res.json(brandsDB);
+router.get("/user", user, auth, (req, res) => {
+  console.log(res.locals);
 });
 
-
+// router.get("/product", (req, res) => {
+// 	res.json(brandsDB);
+// });
 
 // Si se encuenta el producto,devuelve un objeto con:
 //brand , el nombre de la marca
 //description, la descripcion de la marca
 //product, el producto entero que corresponde a esa marca
 router.get("/product/:brand/:productId?", (req, res) => {
-
+  const { brand, productId } = req.params;
+  if (productId) {
+    const ProductsBranch = brandsDB.find((b) => b.name === brand);
+    const product = ProductsBranch.products.find(
+      (b) => b.id === parseInt(productId)
+    );
+    res.json({
+      brand: ProductsBranch.name,
+      description: ProductsBranch.description,
+      product,
+    });
+  } else {
+    const ProductsBranch = brandsDB.find((b) => b.name === brand);
+    res.json(ProductsBranch);
+  }
 });
 
 /**
@@ -58,7 +65,18 @@ http://localhost:3000/api/product
  * */
 
 router.post("/product", (req, res) => {
-
+  const { name, description } = req.body;
+  if (name && description) {
+    const newProduct = {
+      id: brandsDB[brandsDB.length - 1].id + 1,
+      name,
+      description,
+    };
+    brandsDB = [...brandsDB, newProduct];
+    res.json({ message: "Marca agregada", brand: `${name}` });
+  } else {
+    res.status(400).json({ message: "información incompleta del producto" });
+  }
 });
 
 /**
@@ -71,26 +89,37 @@ http://localhost:3000/api/product/2
  * de la brand por el nombre que llega por body
  */
 router.put("/product/:id", (req, res) => {
-	
+  const { id } = req.params;
+  const { brand } = req.body;
+  if (brand) {
+    let brandProducts = brandsDB.find((b) => b.id === parseInt(id));
+    brandProducts.name = brand;
+    res.json({ message: "Producto Actualizado" });
+  }
 });
-
 
 /**
 		DELETE
 http://localhost:3000/api/product/1
  */
 
-
 /**
  * Este método debe poder eliminar un producto
  */
 router.delete("/product/:id", (req, res) => {
-	
+  const { idBranch } = req.body;
+  const { id } = req.params;
+  let brandProducts = brandsDB.find((b) => b.id === parseInt(idBranch));
+  let otherBrands = brandsDB.filter((b) => b.id !== parseInt(idBranch));
+  let productsBrand = brandProducts.products.filter(
+    (p) => p.id !== parseInt(id)
+  );
+  const product = brandProducts.products.find((p) => p.id === parseInt(id));
 
+  brandProducts.products = productsBrand;
+  brandsDB = { ...otherBrands, brandProducts };
+  res.json({ message: "Producto Eliminado", product });
 });
-
-
-
 
 //expressjs.com/es/starter/hello-world.html
 module.exports = { router };
